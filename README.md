b76e2171fed591e6ab962432a51750b7aef9e2be
Jan <github@sintho.com>
2016-10-16 15:39:39 +0200

    small change to the path evaluation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Github-redirect-to-image
 redirects from the imgur image page to the image itself
 
-[Click here to install] (https://github.com/Azlond/Imgur-redirect-to-image/raw/master/RTI.user.js)
+[Click here to install (requires Greasemonkey)] (https://github.com/Azlond/Imgur-redirect-to-image/raw/master/RTI.user.js) 
--- RTI.user.js
+++ RTI.user.js
@@ -3,16 +3,20 @@
 // @name        Imgur redirect to image
 // @namespace   https://github.com/Azlond/Imgur-redirect-to-image
 // @include     https://imgur.com/*
+// @exclude     https://imgur.com/about
 // @exclude     https://imgur.com/privacy
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
 
 var URL = window.location + ""; // current URL
-var path = URL.slice(18, URL.length - 1); //strip the https://imgur.com/
-if (path.length > 4) { //hacky workaround so I don't have to manually exclude all imgur pages like /jobs or /about
-	if (!(/\?+|\//.test(path))) { // see if the path contains "?" or "/" - if yes, we're not interested
+var path = URL.slice(18, URL.length); //strip the https://imgur.com/
+if (path.charAt(path.length - 1) === "/") { //strip the path of a potential slash "/" at the end
+	path = path.slice(0,path.length-1);
+}
+if (path.length === 5 || path.length === 7) { //hacky workaround, imageIDs appear to be either 5 or 7 characters long
+	if (!(/\?+|\//.test(path))) { // see if the path contains "?" or "/", just in case
 		try { // try to catch potential errors
 			var linkElements = document.getElementsByTagName("link"); // collect all <link>-elements from the source code 
 			for (var i = 0; i < linkElements.length; i++) { // go through all object we just collected 
